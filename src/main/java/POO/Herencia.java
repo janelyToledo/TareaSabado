91ed48c33703b178f51df5ea62101a8a0293356f
allan <allan@DESKTOP-VET2ECP>
2024-11-09 14:02:23 -0600

    Actualizacion a la clase perro

---
+++ src/main/java/POO/Herencia.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package POO;
+
+/**
+ *
+ * @author allan
+ */
+public class Herencia {
+
+    public static void main(String[] args) {
+        Perro p = new Perro("Color","Negro","Si","Si","Si",4);
+        p.Sonido();
+        
+        
+         Gato g = new Gato("Blanco","Si","Si","Si",4);
+        g.Sonido();
+    }
+    }
+
